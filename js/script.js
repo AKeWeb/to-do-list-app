560ae98b042eb01da4e166efce02ebd87cea15c3
Andreas W. Keller <131688400+AKeWeb@users.noreply.github.com>
2023-06-05 13:57:28 +0200

    jQuery code used

---
+++ js/script.js
@@ -0,0 +1,35 @@
+function newItem() {
+  //1. Adding a new item to the list of items:
+  let li = $("<li></li>");
+  let inputValue = $("#input").val();
+  li.append(inputValue);
+
+  if (inputValue === "") {
+    alert("You must write something!");
+  } else {
+    $("#list").append(li);
+  }
+
+  //2. Crossing out an item from the list of items:
+  function crossOut() {
+    li.toggleClass("strike");
+  }
+
+  li.on("dblclick", function crossOut() {
+    li.toggleClass("strike");
+  });
+
+  //3a). Adding the delete button "X":
+  let crossOutButton = $("<crossOutButton>X</crossOutButton>");
+  li.append(crossOutButton);
+
+  //3b). Adding CLASS DELETE (DISPLAY: NONE) from the css:
+  function deleteListItem() {
+    li.addClass("delete");
+  }
+  crossOutButton.on("click", deleteListItem);
+
+  // 4. Reordering the items:
+
+  $("#list").sortable();
+}
